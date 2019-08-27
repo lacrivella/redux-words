74ed5228ad9f4c07f054166898b2dbe99787787b
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-27 16:03:48 -0700

    created UPDATE-SEARCH-TERM and added it to reducer

--- src/reducers/wordsReducer.js
+++ src/reducers/wordsReducer.js
@@ -1,9 +1,15 @@
 import words from '../fixtures/words.json';
+import { UPDATE_SEARCH_TERM } from '../actions/wordsActions';
 
-export const initialState = [...words];
+export const initialState = {
+  dictionary: [...words],
+  searchTerm: ''
+};
 
 export default function wordsReducer(state = initialState, action) {
   switch(action.type) {
+    case UPDATE_SEARCH_TERM:
+      return { ...state, searchTerm: action.payload };
     default: return state;
   }
 }
