14d7b05ee4d8ea54f10a0cd02cf1608c3d7b7424
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-27 15:20:10 -0700

    added routes to my app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Words from './words/Words';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import TopWords from '../containers/words/TopWords';
 
 export default function App() {
   return (
-  <>
-  <TopWords />
-  </>
+    <Router>
+      <Switch>
+        <Route path="/words/:cout" component={TopWords} />
+        <Route exact path="/" component={TopWords} />
+      </Switch>
+    </Router>
   );
 }
