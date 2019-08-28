ca0c8872ad7b6fb17e74bb88e002eaa80e772f72
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-28 09:56:58 -0700

    fixed errors and typos in searchform

--- src/components/words/SearchForm.js
+++ src/components/words/SearchForm.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function SearchForm({ searchTerm, handleOnChange }) {
-  <input type="text" value={searchTerm} onChange={handleOnChange} />;
+  return (
+  <>
+  <input type="text" value={searchTerm} onChange={handleOnChange} />
+  </>
+  );
 }
 
 SearchForm.propTypes = {
   searchTerm: PropTypes.string.isRequired,
-  handleOnChanges: PropTypes.func.isRequired
+  handleOnChange: PropTypes.func.isRequired
 };
 
 export default SearchForm;
