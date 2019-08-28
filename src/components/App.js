6c4b253e78d5e82761bcbd45eaafcda3fd8ec95d
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-28 10:02:27 -0700

    updated app to include searchable

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
-import TopWords from '../containers/words/TopWords';
+import Searchable from '../components/words/Searchable';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/words/:count" component={TopWords} />
-        <Route exact path="/" component={TopWords} />
+        <Route path="/words/:count" component={Searchable} />
+        <Route exact path="/" component={Searchable} />
       </Switch>
     </Router>
   );
